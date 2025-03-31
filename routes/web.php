944acf46d82c8383a6d6271184ede5f48b5d30e8
amdymstfa <amdymoustapha011@gmail.com>
2025-03-31 14:06:08 +0000

    Update github repo

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+
  
 // publics routes 
 Route::get('/', function () {
     return view('pages.welcome');
 });
 Route::get('login', [LoginController::class, 'ShowLoginForm']);
+Route::get('login', [RegisterController::class, 'ShowRegisterForm']);
