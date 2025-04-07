f3fe31ef75cee40912e24c3add7325149120f88c
amdymstfa <amdymoustapha011@gmail.com>
2025-04-07 23:29:01 +0100

    update field type of age

--- database/migrations/2025_04_05_193721_add_fields_to_users_tables.php
+++ database/migrations/2025_04_05_193721_add_fields_to_users_tables.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('state')->nullable();
             $table->string('grade')->nullable();
-            $table->integer('age')->nullable();
+            $table->string('age')->nullable();
             $table->string('club')->nullable();
         });
     }
