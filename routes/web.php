9f2279211e2fda9e3c79d0e4ec61a890fe5048fa
amdymstfa <amdymoustapha011@gmail.com>
2025-04-14 12:15:13 +0100

    Feat: handle routes for pages

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,32 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RefereeController;
+use App\Http\Controllers\AthleteController;
+
 
 
  
 // publics routes 
 Route::get('/', function () {
-    return view('pages.welcome');
+    return view('welcome');
 });
 Route::get('login', [LoginController::class, 'ShowLoginForm'])->name('login');
 Route::get('register', [RegisterController::class, 'ShowRegisterForm'])->name('register');
-Route::get('/athlete/dashboard', [LoginController::class, 'index'])->name('pages.athlete.dashboard');
-Route::get('/admin/dashboard', [LoginController::class, 'index'])->name('pages.admin.dashboard');
-Route::get('/referee/dashboard', [LoginController::class, 'index'])->name('pages.referee.dashboard');
 
 
+// Route pour l'admin
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+})->name('admin');
 
+// Route pour l'arbitre
+Route::get('/referee', function () {
+    return view('referee.dashboard');
+})->name('referee');
+
+// Route pour l'athlète
+Route::get('/athlete', function () {
+    return view('athlete.dashboard');
+})->name('athlete');
