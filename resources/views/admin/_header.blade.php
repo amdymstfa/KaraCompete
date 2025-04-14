c6a09d14c66fe6198ef96874b6dfe49fc3279c25
amdymstfa <amdymoustapha011@gmail.com>
2025-04-14 15:41:51 +0100

    Feat(front): Admin panel

---
+++ resources/views/admin/_header.blade.php
@@ -0,0 +1,35 @@
+<header class="bg-white border-b border-gray-200 p-4">
+    <div class="flex justify-between items-center">
+      <div class="flex items-center">
+        <!-- Hamburger menu pour mobile -->
+        <button @click="mobileMenuOpen = true" class="mr-4 md:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+        <h2 class="text-lg font-medium" x-text="
+          activeTab === 'users' ? 'Users Management' : 
+          activeTab === 'Arbitors' ? 'Arbitors Management' : 
+          activeTab === 'Jurys' ? 'Jurys Management' : 
+          'Competitions Management'
+        "></h2>
+      </div>
+      <div class="flex items-center space-x-2 md:space-x-4">
+        <button class="p-2 rounded-full hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+          </svg>
+        </button>
+        <button class="p-2 rounded-full hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
+          </svg>
+        </button>
+        <button class="hidden md:block p-2 rounded-full hover:bg-gray-100">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  </header>
