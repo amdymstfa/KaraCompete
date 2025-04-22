6a6ee03d2a57ba24b283a19566bfee4eb1c6882c
amdymstfa <amdymoustapha011@gmail.com>
2025-04-22 14:58:12 +0100

    Register bindings for UserRepositoryInterface and UserServiceInterface in AppServiceProvider to enable dependency injection

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\UserRepositoryInterface;
+use App\Interfaces\UserServiceInterface;
+use App\Repositories\UserRepository;
+use App\Services\UserService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            UserRepositoryInterface::class, UserRepository::class
+        );
+
+        $this->app->bind(
+            UserServiceInterface::class, UserService::class
+        );
     }
 
     /**
