b233368ee72da55b45dd05634de2d84eee107b3c
amdymstfa <amdymoustapha011@gmail.com>
2025-04-22 14:22:50 +0100

    Feat:Configure api routes of users

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
-
+use App\Http\Controllers\API\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/registration', [RegisterController::class, 'registrationUser']);
 Route::post('/login', [LoginController::class, 'authenticate']);
+Route::apiResource('users', UserController::class)->except(['store']);
