ac97c0f8d6d0638565b979109dc3a8dca9e1be11
amdymstfa <amdymoustapha011@gmail.com>
2025-04-29 12:07:14 +0100

    import js of admin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs'
-
+import { adminPanel } from './admin-panel.js';
 window.Alpine = Alpine
-
+Alpine.data('adminPanel', adminPanel);
 Alpine.start()
 
